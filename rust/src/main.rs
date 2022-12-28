e95e74141d404cee3f2e297ebb2b875874353b7e
3tilley <1138504+3tilley@users.noreply.github.com>
2022-12-28 03:32:48 +0000

    Pin work

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,20 @@
 #[macro_use] extern crate rocket;
 
 use std::net::Ipv4Addr;
+use std::thread;
+use std::time::Duration;
 use rocket::Config;
+use rppal::gpio::Gpio;
+use rppal::system::DeviceInfo;
+
+// These are Broadcom pins (BCM), they correspond to physical pins 15 and 16 respectively
+// https://electronicsmith.com/raspberry-pi-pinout-for-all-models/
+const GPIO_BUZZER: u8 = 22;
+const GPIO_LED: u8 = 23;
 
 #[get("/")]
-fn index() -> &'static str {
-    "This is the Rust service"
+fn index() -> String {
+    format!("This is the Rust service running on {}", DeviceInfo::new().unwrap()?.model())
 }
 
 #[get("/hello")]
@@ -25,6 +34,12 @@ fn beep_brief() -> &'static str {
 
 #[get("/blink")]
 fn blink() -> &'static str {
+    let mut pin = Gpio::new().unwrap().get(GPIO_LED).unwrap().into_output();
+
+    // Blink the LED by setting the pin's logic level high for 500 ms.
+    pin.set_high();
+    thread::sleep(Duration::from_millis(500));
+    pin.set_low();
     "Blink"
 }
 
@@ -37,6 +52,7 @@ fn blink_times(times: u8) -> String {
 fn rocket() -> _ {
     let config = Config {
         address: Ipv4Addr::new(0, 0, 0, 0).into(),
+        port: 80,
         ..Config::debug_default()
     };
 
