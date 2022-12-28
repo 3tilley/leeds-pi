cc0922d2b79c863079f3b1a4bd75e33bb18a6a8d
3tilley <1138504+3tilley@users.noreply.github.com>
2022-12-28 03:03:04 +0000

    Routes and config update

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,8 @@
 #[macro_use] extern crate rocket;
 
+use std::net::Ipv4Addr;
+use rocket::Config;
+
 #[get("/")]
 fn index() -> &'static str {
     "This is the Rust service"
@@ -15,7 +18,27 @@ fn hello_name(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
+#[get("/beep")]
+fn beep_brief() -> &'static str {
+    "Beep"
+}
+
+#[get("/blink")]
+fn blink() -> &'static str {
+    "Blink"
+}
+
+#[get("/blink/<times>")]
+fn blink_times(times: u8) -> String {
+    format!("Blink {} times", times)
+}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, hello, hello_name])
+    let config = Config {
+        address: Ipv4Addr::new(0, 0, 0, 0).into(),
+        ..Config::debug_default()
+    };
+
+    rocket::custom(config).mount("/", routes![index, hello, hello_name])
 }
