95d7c4f058fdeed9a7ad1e747905cf6620fb4133
Pi <pi@pi.com>
2022-12-29 00:18:10 +0000

    Some updates

--- python/receive.py
+++ python/receive.py
@@ -9,22 +9,32 @@ logging.basicConfig(
     format="%(asctime)s:%(levelname)s:%(name)s:%(funcName)s:%(message)s",
     datefmt="%Y-%m-%dT%H:%M:%S%z"
 )
+# Original baud bands, nothing in the higher freqs was found
+#SUPPORTED_BAUD = [
+    #600,
+#1200,
+    #2400,
+#4800,
+  #9600,
+ #14400,
+ #19200,
+  #28800,
+  #38400,
+  #57600,
+  #76800,
+  #115200,
+  #250000,
+#]
 
-SUPPORTED_BAUD = [
-    "600",
-"1200",
-    "2400",
-"4800",
-  "9600",
- "14400",
- "19200",
-  "28800",
-  "38400",
-  "57600",
-  "76800",
-  "115200",
-  "250000",
+NEW_BANDS = [
+    50,
+    110,
+    300,
+    600,
+    1200
 ]
+
+SEARCH_TIMEOUT = 5
 
 p = Path() / "data.csv"
 
@@ -32,15 +42,16 @@ with p.open("a") as f:
 
     with cc1101.CC1101() as transceiver:
         transceiver.set_base_frequency_hertz(433.92e6)
-        for baud in SUPPORTED_BAUD:
-            transceiver.set_symbol_rate_baud(baud)
-            for sync_mode in [e for e in cc1101.SyncMode]:
-                transceiver.set_sync_mode(sync_mode, True)
-                print(transceiver)
-                #transceiver.transmit(b"\x01\xff\x00 message")
-                while True:
+        while True:
+            for baud in NEW_BANDS:
+                transceiver.set_symbol_rate_baud(baud)
+                #for sync_mode in [e for e in cc1101.SyncMode]:
+                for sync_mode in [cc1101.SyncMode.NO_PREAMBLE_AND_SYNC_WORD]:
+                    transceiver.set_sync_mode(sync_mode)
+                    print(transceiver)
+                    #transceiver.transmit(b"\x01\xff\x00 message")
                     start_time = datetime.datetime.now()
-                    packet = transceiver._wait_for_packet(timeout_seconds=10,gdo0_gpio_line_name="GPIO24".encode())
+                    packet = transceiver._wait_for_packet(timeout_seconds=SEARCH_TIMEOUT,gdo0_gpio_line_name="GPIO24".encode())
                     if packet:
                         end = datetime.datetime.now()
                         d = (end - start_time).total_seconds()
